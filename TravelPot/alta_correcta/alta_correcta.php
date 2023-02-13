b17d0ed98046e4e0eaf9d27bece29b4588f7ab56
Ivan Perez <ivan_psh@hotmail.com>
2023-02-13 10:59:35 +0100

    New update

--- TravelPot/alta_correcta/alta_correcta.php
+++ TravelPot/alta_correcta/alta_correcta.php
@@ -1,27 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Alta Correcta</title>
 	<link rel="stylesheet" href="css/styles.css">
 </head>
+
 <body>
 
-	<?php
-        $carga_xml = simplexml_load_file("../prueba.xml");
-		$no_root = $xml->usuarios->addChild('no_root');
-
-		$no_root->addChild('nombre', $_POST['nombre']);
-		$no_root->addChild('apellidos', $_POST['apellidos']);
-		$no_root->addChild('direccion', $_POST['direccion']);
-		$no_root->addChild('poblacion', $_POST['poblacion']);
-		$no_root->addChild('provincia', $_POST['provincia']);
-		$no_root->addChild('email', $_POST['email']);
-		$no_root->addChild('usuario', $_POST['usuario']);
-		$no_root->addChild('contrasenya', $_POST['contrasenya']);
-
-	?>
 	<div id="full_container">
 		<header>
 			<nav>
@@ -33,7 +21,7 @@
 							</div>
 						</td>
 						<td style="text-align: right;">
-							<a href="../index_esp.html"class="volver">Aquí irá el nombre de usuario y el saludo</a>
+							<a href="../index_esp.html" class="volver">Bienvenido <?php echo $_POST['nombre']; ?></a>
 						</td>
 					</tr>
 				</table>
@@ -47,13 +35,37 @@
 							<legend>¡Alta correcta!</legend>
 							<p>¡Enhorabuena!<br> Te has dado de alta correctamente.</p>
 							<img src="../multimedia/check.png" alt="icono-correcto">
-					        <input type="submit" value="Volver al inicio" class="volver-iniciarSesion">
+							<input type="submit" value="Volver al inicio" class="volver-iniciarSesion">
 							<input type="button" value="Iniciar sesión" class="volver-iniciarSesion" onclick="location.href='../inicio_sesion/inicio_sesion.html'">
 						</fieldset>
 					</form>
 				</div>
 			</section>
+			<section>
+				<?php
+				$carga_xml = simplexml_load_file("../../travelpot.xml");
+				$travelpot = $xml->travelpot;
+
+				$no_root = $travelpot->usuarios->addChild('no_root');
+
+				$no_root->addChild('nombre', $_POST['nombre']);
+				// $no_root->addChild('apellidos', $_POST['apellidos']);
+				// $no_root->addChild('direccion', $_POST['direccion']);
+				// $no_root->addChild('poblacion', $_POST['poblacion']);
+				// $no_root->addChild('provincia', $_POST['provincia']);
+				// $no_root->addChild('email', $_POST['email']);
+				// $no_root->addChild('usuario', $_POST['usuario']);
+				// $no_root->addChild('contrasenya', $_POST['contrasenya']);
+
+				$carga_xml->asXML("../../travelpot.xml")
+
+				?> 
+				<p>FFfff</p>
+				<p>Bienvenido <?php echo $_POST['nombre']; ?></p>
+
+			</section>
 		</main>
 	</div>
 </body>
+
 </html>
